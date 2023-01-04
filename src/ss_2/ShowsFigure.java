8cc3ac1ea0b724adc5a11c496bf060b06542f873
c1122g1-lanh <lanh7108@gmail.com>
2023-01-04 15:37:42 +0700

    up

--- src/ss_2/ShowsFigure.java
+++ src/ss_2/ShowsFigure.java
@@ -3,27 +3,27 @@ package ss_2;
 import java.util.Scanner;
 
 public class ShowsFigure {
-    static String sao = "*";
+    static String star = "*";
 
-    public static void retangular() {
+    public static void retangle() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 7; j++) {
-                System.out.print(sao);
+                System.out.print(star);
             }
             System.out.println("");
         }
     }
 
-    public static void triangular() {
+    public static void triangle() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print(sao + " ");
+                System.out.print(star + " ");
             }
             System.out.println(" ");
         }
         for (int i = 4; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                System.out.print(sao + " ");
+                System.out.print(star + " ");
             }
             System.out.println(" ");
         }
@@ -32,8 +32,8 @@ public class ShowsFigure {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        retangular();
-        triangular();
+        retangle();
+        triangle();
 
     }
 }
